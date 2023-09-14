dcf586b3611855eca81d85c854ef7472f4c1fc7a
demid <demid.krasnovidov@gmail.com>
2023-09-14 15:56:57 +0300

    Removed unused imports

---
+++ src/temporary/data/questionData.ts
@@ -0,0 +1,15 @@
+import {IQuestion} from "../../shared/interfaces/IQuestion";
+import secondPic from "../../shared/images/TempPic2.jpg";
+
+export const hardcodedQuestion : IQuestion = {
+    id: 1,
+    title: 'Название два',
+    description: 'Описание два',
+    username: 'Димас123',
+    time: 45,
+    stars: 5,
+    timesFinished: 10,
+    steps: 3,
+    coordinates: [[47.21706929180965, 39.7120816141971], [47.26830478974905, 39.722743461852545], [47.213592874536324, 39.72291512322952]],
+    imageUrl: secondPic
+}
---
+++ src/temporary/data/questionListData.ts
@@ -0,0 +1,78 @@
+import {IQuestion} from "../../shared/interfaces/IQuestion";
+import pic from "../../shared/images/TemporaryPicture.jpg";
+import secondPic from "../../shared/images/TempPic2.jpg";
+
+export const hardcodedQuestions: IQuestion[] = [
+    {
+        id: 0,
+        title: 'Название',
+        description: 'Описание',
+        username: 'Пользователь',
+        time: 90,
+        stars: 4.3,
+        timesFinished: 54,
+        steps: 6,
+        coordinates: [[1,1]],
+        imageUrl: pic
+    },
+    {
+        id: 1,
+        title: 'Название два',
+        description: 'Описание два',
+        username: 'Димас123',
+        time: 45,
+        stars: 5,
+        timesFinished: 10,
+        steps: 10,
+        coordinates: [[1,1]],
+        imageUrl: secondPic
+    },
+    {
+        id: 0,
+        title: 'Название',
+        description: 'Описание',
+        username: 'Пользователь',
+        time: 90,
+        stars: 1,
+        timesFinished: 54,
+        steps: 6,
+        coordinates: [[1,1]],
+        imageUrl: pic
+    },
+    {
+        id: 1,
+        title: 'Название два',
+        description: 'Описание два',
+        username: 'Димас123',
+        time: 45,
+        stars: 2.6,
+        timesFinished: 10,
+        steps: 10,
+        coordinates: [[1,1]],
+        imageUrl: secondPic
+    },
+    {
+        id: 0,
+        title: 'Название',
+        description: 'Описание',
+        username: 'Пользователь',
+        time: 90,
+        stars: 2.45,
+        timesFinished: 54,
+        steps: 6,
+        coordinates: [[1,1]],
+        imageUrl: pic
+    },
+    {
+        id: 1,
+        title: 'Название два',
+        description: 'Описание два',
+        username: 'Димас123',
+        time: 45,
+        stars: 4.44,
+        timesFinished: 10,
+        steps: 10,
+        coordinates: [[1,1]],
+        imageUrl: secondPic
+    },
+]
